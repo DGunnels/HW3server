7e52ecd0234ec13e4a8c6146cc2059546f8a19ca
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-01 15:35:04 -0600

    C'mon already.

--- Users.js
+++ Users.js
@@ -9,7 +9,7 @@ const connectOptions = {
     user: process.env.dbUser,
     pass: process.env.dbPass,
     dbName: 'HW3'
- 
+
 }
 
 
@@ -22,7 +22,9 @@ mongoose.connect(process.env.DB, connectOptions, function (error) {
 // user schema
 var UserSchema = new Schema({
     username: { type: String, required: true, index: { unique: true } },
-    email: { type: String, required: true },
+    email: {
+        type: String, required: true, index: { unique: true }
+    },
     password: { type: String, required: true, select: false }
 });
 
@@ -33,23 +35,27 @@ UserSchema.pre('save', function (next) {
     // hash the password only if the password has been changed or user is new
     if (!user.isModified('password')) return next();
 
-    // generate the hash
-    bcrypt.hash(user.password, saltRound).then(function (err, hash) {
+    // say hi to the salt
+    bcrypt.genSalt(saltRound, function (err, salt) {
         if (err) return next(err);
 
-        // change the password to the hashed version
-        user.password = hash;
-        next();
-    });
+        // hashtime with salt
+        bcrypt.hash(user.password, salt, function9err, hash){
+            if (err) return next(err);
+
+            //override clear text with hash
+            user.password = hash;
+            next();
+        });
 });
 
-UserSchema.methods.comparePassword = function (password, callback) {
-    var user = this;
-
-    bcrypt.compare(password, user.password, function (err, isMatch) {
-        callback(isMatch);
+UserSchema.methods.comparePassword = function (passwordEntered, callback) {
+    bcrypt.compare(passwordEntered, this.password, function (err, isMatch) {
+        if (err) return callback(err);
+        callback(null, isMatch);
     });
 };
+  
 
 // return the model
 module.exports = mongoose.model('User', UserSchema);
