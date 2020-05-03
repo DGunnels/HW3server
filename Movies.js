c3e96185be3912e96153f24173df8208a6401c74
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-02 23:24:35 -0600

    Update Movies.js

--- Movies.js
+++ Movies.js
@@ -7,7 +7,7 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-
+  
 mongoose.connect(process.env.DB, { useNewUrlParser: true });
 mongoose.set('useCreateIndex', true);
 
