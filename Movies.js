9aec50d952cdb8a93bcda04d1b5d657a1e600abe
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-02 23:30:19 -0600

    Updates/Removals/etc

--- Movies.js
+++ Movies.js
@@ -1,22 +1,30 @@
 
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
-var MongoClient = require('mongodb').MongoClient;
-var url = "mongodb://localhost/webapi";
+var MongoClient=require('mongodb').MongoClient;
+var url="mongodb://localhost/webapi";
 var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
+/*MongoClient.connect(url,function(err,db){
+    if (err)throw err;
+    var dbo=db.db("mydb");
+    dbo.createCollection("movies",function(err,res){
+        if(err)throw err;
+        console.log("Collection created!");
+        db.close();
+    });
+});*/
 
-    
-mongoose.connect(process.env.DB, { useNewUrlParser: true });
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
 mongoose.set('useCreateIndex', true);
 
 // Movie schema
 var movieSchema = new Schema({
-    Title: { type: String, required: true, index: { unique: true } },
+    Title: { type: String, required: true, index: { unique: true }},
     Year: { type: String },
-    Genre: { type: String, required: true, enum: ['Action', 'Adventure', 'Comedy', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Drama', 'Western'] },
-    Actors: { type: Array, Name: { type: String }, Character: { type: String } },
+    Genre: { type: String, required: true, enum:['Action','Adventure','Comedy','Fantasy','Horror','Mystery','Thriller','Drama','Western']},
+    Actors : { type : Array,Name:{type:String},Character:{type:String} },
 });
 
 
--- Users.js
+++ Users.js
@@ -4,25 +4,25 @@ var bcrypt = require('bcrypt-nodejs');
 
 mongoose.Promise = global.Promise;
 
-mongoose.connect(process.env.DB, { useNewUrlParser: true });
+mongoose.connect(process.env.DB, { useNewUrlParser: true } );
 mongoose.set('useCreateIndex', true);
 
 // user schema
 var UserSchema = new Schema({
     name: String,
-    username: { type: String, required: true, index: { unique: true } },
+    username: { type: String, required: true, index: { unique: true }},
     password: { type: String, required: true, select: false }
 });
 
 // hash the password before the user is saved
-UserSchema.pre('save', function (next) {
+UserSchema.pre('save', function(next) {
     var user = this;
 
     // hash the password only if the password has been changed or user is new
     if (!user.isModified('password')) return next();
 
     // generate the hash
-    bcrypt.hash(user.password, null, null, function (err, hash) {
+    bcrypt.hash(user.password, null, null, function(err, hash) {
         if (err) return next(err);
 
         // change the password to the hashed version
@@ -31,11 +31,11 @@ UserSchema.pre('save', function (next) {
     });
 });
 
-UserSchema.methods.comparePassword = function (password, callback) {
+UserSchema.methods.comparePassword = function(password, callback) {
     var user = this;
 
-    bcrypt.compare(password, user.password, function (err, isMatch) {
-        callback(isMatch);
+    bcrypt.compare(password, user.password, function(err, isMatch) {
+       callback(isMatch) ;
     });
 };
 
