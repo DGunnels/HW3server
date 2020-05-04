21f934bb1ca69c187c12c032a24c216fa8b6a082
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-04 02:38:58 -0600

    w;alkjtg;aw

--- server.js
+++ server.js
@@ -245,7 +245,7 @@ router.route('/reviews/:id')
 
 router.route('/reviews')
     .post(authJwtController.isAuthenticated, function (req, res) {
-        Movie.findOne({ Title: req.body.MovieTitle }).exec(function (err, movie) {
+        Movie.findOne({ _id: req.body.id }).exec(function (err, movie) {
             if (err) return res.send(err);
             //If the movie exists, add new reviews
             if (movie !== null) {
