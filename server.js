e216234cda46c04fbee2eb6889e67e2049f639ad
Derek Gunnels <45681663+DGunnels@users.noreply.github.com>
2020-05-04 02:01:29 -0600

    s

--- server.js
+++ server.js
@@ -105,7 +105,7 @@ router.post('/signin', function (req, res) {
             if (isMatch) {
                 var userToken = { id: user._id, username: user.username };
                 var token = jwt.sign(userToken, process.env.SECRET_KEY);
-                res.json({ success: true, token: 'JWT ' + token, usernameToken: userToken.username });
+                res.json({ success: true, token: 'JWT ' + token });
             }
             else {
                 res.status(401).send({ success: false, message: 'Authentication failed.' });
